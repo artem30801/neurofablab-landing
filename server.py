fac7eea2e84f361120cb695f764ff574613bd03b
Artem30801 <artem30801@gmail.com>
2019-07-21 01:57:16 +0300

    server fix

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 import sanic
 from sanic import Sanic
 from sanic import response
@@ -12,9 +13,11 @@ app.static('/assests', assests_path)
 app.static('/style.css', web_path+"style.css", name='css')
 app.static('/script.js',  web_path+"script.js", name='js')
 
+
 @app.route("/")
 async def index(request):
     return response.redirect('/landing')
+
 
 @app.route("/landing")
 async def reader_page(request):
@@ -25,5 +28,6 @@ def start():
     ip = '0.0.0.0'
     app.run(host=ip, port=80)
 
+
 if __name__ == "__main__":
-    app.start()
+    start()
