913fcf16c952d08ea86cdf113a79d9a42053e8a8
Artem30801 <artem30801@gmail.com>
2019-07-21 00:58:17 +0300

    dd

--- web/action.php
+++ web/action.php
@@ -2,7 +2,7 @@
  $name=$_POST['name'];
  $tel=$_POST['phone'];
  #$rs =  mail('avtolet969@gmail.com', 'NCognoLab', 'Имя: '.$name.'Телефон : '.$tel, 'From: fewfinfo@ncognolab.ru');
- $txt='ХУЙ';
+ $txt='XUI';
  $txt = str_replace("\n.", "\n..", $txt);
  $rs= mail('avtolet969@gmail.com', 'My Subject', $txt);
  if ($rs) {
