ebe2475c9fa0a91fe6561469ba7bd307c592d005
Artem30801 <artem30801@gmail.com>
2019-07-21 00:12:00 +0300

    fix

--- web/action.php
+++ web/action.php
@@ -2,12 +2,12 @@
  $name=$_POST['name'];
  $tel=$_POST['phone'];
  #$rs =  mail('avtolet969@gmail.com', 'NCognoLab', 'Имя: '.$name.'Телефон : '.$tel, 'From: fewfinfo@ncognolab.ru');
- $rs= mail('avtolet969@gmail.com', 'My Subject', 'ХУЙ');
+ $rs= mail('zaizevt00@yandex.ru', 'My Subject', 'ХУЙ');
  if ($rs) {
   $result="Успешно!Мы перезвоним в ближайшее время!";
  }
  else{
-  $result = 'avtolet969@gmail.com'.'NCognoLab'.'Имя: '.$name.'Телефон : '.$tel.'From: info@ncognolab.com';
+  $result = '...';
  }
     echo json_encode(array(
         'result' => $result
