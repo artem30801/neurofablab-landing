03172bb3d60a104abfa02719d0aa86b99b38f4f5
Artem30801 <artem30801@gmail.com>
2019-07-21 01:20:12 +0300

    fix

--- web/action.php
+++ web/action.php
@@ -1,16 +1,22 @@
 <?php
+require 'vendor/autoload.php';
  $name=$_POST['name'];
  $tel=$_POST['phone'];
- #$rs =  mail('avtolet969@gmail.com', 'NCognoLab', 'Имя: '.$name.'Телефон : '.$tel, 'From: fewfinfo@ncognolab.ru');
- $txt='XUI';
- $txt = str_replace("\n.", "\n..", $txt);
- $rs= mail('avtolet969@gmail.com', 'My Subject', $txt);
- if ($rs) {
-  $result="Успешно!Мы перезвоним в  ближайшее время!";
- }
- else{
-  $result = 'no';
- }
+ $to = "zaizevt00@yandex.ru"; // Update with email.
+ $subject = "NCogno";
+ 
+ // Create new fields here for each field in your form.
+ // Ideally you would want some validation here too.
+
+ $headers = "From: ncogno_forms@ncognolab.com\n";
+
+ $message = "Name : $name\nPhone: $tel";
+
+ $result= "Form Sent";
+ mail($to, $subject, $message, $headers);
+} else {
+ $result= "Form failed to send. Please contact us directly.";
+}
     echo json_encode(array(
         'result' => $result
     ));
