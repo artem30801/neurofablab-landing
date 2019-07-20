05ecd6cb3d01272337da67b17d8b882fdb17dccc
Artem30801 <artem30801@gmail.com>
2019-07-21 00:15:30 +0300

    fix

--- web/action.php
+++ web/action.php
@@ -2,10 +2,11 @@
  $name=$_POST['name'];
  $tel=$_POST['phone'];
  #$rs =  mail('avtolet969@gmail.com', 'NCognoLab', 'Имя: '.$name.'Телефон : '.$tel, 'From: fewfinfo@ncognolab.ru');
- $s='ХУЙ';
- $rs= mail('avtolet969@gmail.com', 'My Subject', $s);
+ $txt='ХУЙ';
+ $txt = str_replace("\n.", "\n..", $txt);
+ $rs= mail('avtolet969@gmail.com', 'My Subject', $txt);
  if ($rs) {
-  $result="Успешно!Мы перезвоним в ближайшее время!";
+  $result="Успешно!Мы перезвоним в  ближайшее время!";
  }
  else{
   $result = 'avtolet969@gmail.com'.'NCognoLab'.'Имя: '.$name.'Телефон : '.$tel.'From: info@ncognolab.com';
