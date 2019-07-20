d8be1d6c0b6ed1c1d48d419659a32ba8d4f7acfe
Artem30801 <artem30801@gmail.com>
2019-07-20 23:51:55 +0300

    fix

--- web/action.php
+++ web/action.php
@@ -6,7 +6,7 @@
   $result="Успешно!Мы перезвоним в ближайшее время!";
  }
  else{
-  $result = $tel;
+  $result = 'avtolet969@gmail.com', 'NCognoLab', 'Имя: '.$name.'Телефон : '.$tel, 'From: info@ncognolab.com';
  }
     echo json_encode(array(
         'result' => $result
